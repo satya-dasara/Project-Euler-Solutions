a11b2456a8faaa429e592fba00bbfe0e64b52d3f
satya-dasara <33514838+satya-dasara@users.noreply.github.com>
2018-02-27 15:51:14 +0530

    Create 003.py

---
+++ Python/003.py
@@ -0,0 +1,21 @@
+#Find the largest prime factor of a number
+
+from math import sqrt
+
+def prime_factor(n):
+    k=[]
+    flag=True
+    n1=n
+    for f in range(2,int(sqrt(n))+1): 
+        if int(n1%f):
+            f=f+int(n%f)
+            continue
+        else:
+            k.append(f)
+            n1=n1/f
+        if f>=n:
+            break
+    return max(k)
+        
+        
+print(prime_factor(600851475143))
